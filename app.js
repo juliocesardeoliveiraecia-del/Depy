7a24c095c8c0dd848dd5728e3a6bd148e384253d
juliocesardeoliveiraecia-del <juliocesardeoliveiraecia@gmail.com>
2026-03-30 11:26:50 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+function startProSync() {
+  setInterval(async () => {
+    const { data } = await supabase
+      .from("users")
+      .select("pro")
+      .eq("id", state.userId)
+      .single();
 
+    if (data?.pro && !state.isPro) {
+      state.isPro = true;
+
+      showSystemMessage("Depy completo desbloqueado.");
+    }
+  }, 4000); // verifica a cada 4 segundos
+}
